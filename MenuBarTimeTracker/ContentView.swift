80af547c30dec1e73b5ec59ff8f50990cf4f8eb9
Aiur <gangrue@gmail.com>
2021-05-27 14:19:59 +0800

    Initial Commit

---
+++ MenuBarTimeTracker/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  MenuBarTimeTracker
+//
+//  Created by Aiur on 27.05.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
